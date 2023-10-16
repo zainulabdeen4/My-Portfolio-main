c846d98e1b32c8b9e9ff3b76b62e7dc6c8ac024c
zainulabdeen4 <zain_master1996@hotmail.com>
2023-10-17 02:31:02 +0500

    Updates

---
+++ precache-manifest.47a8ff77a1a8b47ceeff6700f249019e.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e861bf29a252d874fda6ee4aedee90d",
+    "url": "/%7BMy-Portfolio-main%7D/index.html"
+  },
+  {
+    "revision": "2a7b941ce8b84496a0f5",
+    "url": "/%7BMy-Portfolio-main%7D/static/css/main.a44205ae.chunk.css"
+  },
+  {
+    "revision": "f90410d32156286ac58a",
+    "url": "/%7BMy-Portfolio-main%7D/static/js/2.063b3d5c.chunk.js"
+  },
+  {
+    "revision": "bacbf760ad79b95a405dc91f36bd0811",
+    "url": "/%7BMy-Portfolio-main%7D/static/js/2.063b3d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a7b941ce8b84496a0f5",
+    "url": "/%7BMy-Portfolio-main%7D/static/js/main.83edf26b.chunk.js"
+  },
+  {
+    "revision": "ee56eca32bd49e030655",
+    "url": "/%7BMy-Portfolio-main%7D/static/js/runtime-main.d6ff5e7d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "baffb94f419c4b01b6bee510c57ac847",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/AmcatLogo.baffb94f.png"
+  },
+  {
+    "revision": "f90b4fcd790878ec1b764867d297986e",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/CusatLogo.f90b4fcd.png"
+  },
+  {
+    "revision": "166bfa04c7869a266276730fd5904e9c",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/EmkanLogo.166bfa04.png"
+  },
+  {
+    "revision": "7c7d143e2625d6332385b5d13f04457c",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GateLogo.7c7d143e.png"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "90773b6158663ab0fe78b32680733677",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GoogleSans-BoldItalic.90773b61.ttf"
+  },
+  {
+    "revision": "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GoogleSans-Italic.0ecddcde.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "8fd3737925e83c87d78a13700ccf9a62",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GoogleSans-MediumItalic.8fd37379.ttf"
+  },
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "a9bde1768ad1dd307cafe1a2360d9610",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/KeralaLogo.a9bde176.png"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "5c77079b53136b0c7605b8a667f277a2",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/OnwardLogo.5c77079b.png"
+  },
+  {
+    "revision": "66af2ecc40515b254d57dde7fd9503ea",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/TcsLogo.66af2ecc.png"
+  },
+  {
+    "revision": "6e4af8f0bf57d6d273e5a2110b5bda51",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/ZeroneLogo.6e4af8f0.png"
+  },
+  {
+    "revision": "06147b6cd88c7346cecd1edd060cd5de",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-brands-400.06147b6c.ttf"
+  },
+  {
+    "revision": "5063b105c7646c8043d58c5289f02cca",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-brands-400.5063b105.eot"
+  },
+  {
+    "revision": "a9c4bb7348f42626454c988dbde1d0a0",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-brands-400.a9c4bb73.svg"
+  },
+  {
+    "revision": "c5e0f14f88a828261ba01558ce2bf26f",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-brands-400.c5e0f14f.woff"
+  },
+  {
+    "revision": "cccc9d29470e879e40eb70249d9a2705",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-brands-400.cccc9d29.woff2"
+  },
+  {
+    "revision": "65b286af947c0d982ca01b40e1fcab38",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-regular-400.65b286af.ttf"
+  },
+  {
+    "revision": "7b9568e6389b1f8ae0902cd39665fc1e",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-regular-400.7b9568e6.svg"
+  },
+  {
+    "revision": "c1a866ec0e04a5e1915b41fcf261457c",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-regular-400.c1a866ec.eot"
+  },
+  {
+    "revision": "c4f508e7c4f01a9eeba7f08155cde04e",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-regular-400.c4f508e7.woff"
+  },
+  {
+    "revision": "f5f2566b93e89391da4db79462b8078b",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-regular-400.f5f2566b.woff2"
+  },
+  {
+    "revision": "0bff33a5fd7ec390235476b4859747a0",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-solid-900.0bff33a5.ttf"
+  },
+  {
+    "revision": "333bae208dc363746961b234ff6c2500",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-solid-900.333bae20.woff"
+  },
+  {
+    "revision": "44d537ab79f921fde5a28b2c1636f397",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-solid-900.44d537ab.woff2"
+  },
+  {
+    "revision": "8e4a6dcc692b3887f9f542cd6894d6d4",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-solid-900.8e4a6dcc.eot"
+  },
+  {
+    "revision": "c2801fb415f03c7b170934769d7b5397",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/fa-solid-900.c2801fb4.svg"
+  },
+  {
+    "revision": "f4fa9f26c46504d96608bd9323c1f79e",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/piaic.f4fa9f26.png"
+  },
+  {
+    "revision": "d884dbe20b02cf2541c48b1c2e283cf0",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/profile_photo.d884dbe2.jpg"
+  },
+  {
+    "revision": "bc9a9542b0eb258250c4462aa143f08e",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/rc.bc9a9542.svg"
+  },
+  {
+    "revision": "0088c1aeb4128b2f7299c487224122db",
+    "url": "/%7BMy-Portfolio-main%7D/static/media/rs.0088c1ae.png"
+  }
+]);
